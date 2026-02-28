1fff3b37560f75368d1cf9189691f399ada73d8e
Kunal Keshari Pattanaik <kunalpattanaik9090@gmail.com>
2026-02-28 22:47:16 +0530

    fixed render port issue

--- api/server.js
+++ api/server.js
@@ -102,8 +102,10 @@ error:String(err)
 START SERVER
 ===================================== */
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
 
-console.log("🔥 Code Engine Running on http://localhost:3000");
+app.listen(PORT, () => {
+
+console.log(`🔥 Code Engine Running on port ${PORT}`);
 
 });
